04555dbec082fe35faa950c6bbc6ebf54f6b9c90
leandrcosta <leandrocostamdc@gmail.com>
2023-04-02 18:42:53 -0300

    Feat: Criação das Models e Migrations [User, Match], e no caso da model Match, foram feitas as referencias[chave estrangeira e primaria]

---
+++ app/backend/src/database/models/Match.Model.ts
@@ -0,0 +1,47 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+
+class Match extends Model {
+  declare id: number;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Match.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  homeTeamId: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  homeTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  awayTeamId: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  awayTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  inProgress: {
+    allowNull: false,
+    type: BOOLEAN,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  underscored: true,
+  timestamps: false,
+});
+
+export default Match;

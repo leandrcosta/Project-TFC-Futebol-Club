f1af61d457ad18497353bc47dfc1a67f2cacdcf3
leandrcosta <leandrocostamdc@gmail.com>
2023-04-03 16:05:18 -0300

    Requisito 02 [ minimo de 5% de testes - Usando TDD]

---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,34 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+import { app } from "../app";
+/* import { Model } from "sequelize"; */
+import Team from "../database/models/Team.Model";
+import mockTeams from './mocks/mock.teams';
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe("GET/teams", () => {
+  describe('Quando a requisição é feita com sucesso', () => {
+    it('Retorna um array de objetos com todos os times cadastrados e statusCode(200)', async () => {
+      const times = [
+        {
+          "id": 1,
+          "teamName": "Avaí/Kindermann"
+        },
+        {
+          "id": 2,
+          "teamName": "Bahia"
+        }
+      ]
+      sinon.stub(Team, 'findAll').resolves(mockTeams as Team[]);
+
+      const response = await chai.request(app).get('/teams');
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.deep.equal(mockTeams)
+    })
+  })
+});

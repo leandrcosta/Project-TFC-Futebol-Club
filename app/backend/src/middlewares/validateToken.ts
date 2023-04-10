ac3dadc8ed19020f806d895c07790554d7454274
leandrcosta <leandrocostamdc@gmail.com>
2023-04-10 16:27:50 -0300

    Implementação do validateToken

---
+++ app/backend/src/middlewares/validateToken.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+import { verifyToken } from '../auth/authFunctions';
+
+const validateToken = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { authorization } = req.headers;// pego o token que veio no autorization
+    if (!authorization) return res.status(401).json({ message: 'Token not found' });
+
+    const payload = verifyToken(authorization);
+    req.body.user = payload; // req transita por todas as rotas
+    console.log(payload);
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Token must be a valid token' });
+  }
+};
+
+export default { validateToken };
